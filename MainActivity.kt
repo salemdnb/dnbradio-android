d1cc8483a1a9e272ce023119b92dcf4ff4098685
salemdnb <hzn.network@gmail.com>
2026-08-14 04:58:01 -0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -9,20 +9,49 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 
 class MainActivity : ComponentActivity() {
+
+    private var player: ExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        player = ExoPlayer.Builder(this).build()
+
+        val mediaItem = MediaItem.fromUri(
+            "https://azura.dnbradio.com/listen/dnbradio/dnbradio_main.mp3"
+        )
+
+        player?.setMediaItem(mediaItem)
+        player?.prepare()
+
         setContent {
-            DNBRadioApp()
+            DNBRadioApp(
+                onPlay = {
+                    player?.play()
+                },
+                onStop = {
+                    player?.pause()
+                }
+            )
         }
+    }
+
+    override fun onDestroy() {
+        player?.release()
+        player = null
+        super.onDestroy()
     }
 }
 
 @Composable
-fun DNBRadioApp() {
+fun DNBRadioApp(
+    onPlay: () -> Unit,
+    onStop: () -> Unit
+) {
 
     var tocando by remember { mutableStateOf(false) }
 
@@ -39,21 +68,39 @@ fun DNBRadioApp() {
             style = MaterialTheme.typography.headlineLarge
         )
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(12.dp))
 
         Text(
-            text = if (tocando) "▶ Rádio tocando" else "⏸ Rádio parada"
+            text = "Drum & Bass 24/7"
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Text(
+            text = if (tocando)
+                "▶ RÁDIO TOCANDO"
+            else
+                "⏸ RÁDIO PARADA"
         )
 
         Spacer(modifier = Modifier.height(30.dp))
 
         Button(
             onClick = {
+                if (tocando) {
+                    onStop()
+                } else {
+                    onPlay()
+                }
+
                 tocando = !tocando
             }
         ) {
             Text(
-                text = if (tocando) "PARAR" else "OUVIR RÁDIO"
+                text = if (tocando)
+                    "PARAR"
+                else
+                    "OUVIR RÁDIO"
             )
         }
     }
